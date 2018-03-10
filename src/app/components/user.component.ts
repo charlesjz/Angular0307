6329d7fe3ad385cb9e42c82a728076765ac04f26
Jiang Zhu <charles.j.zhu@gmail.com>
2018-03-09 20:50:45 -0500

    '0309'

---
+++ src/app/components/user.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: `user.component.html`,
+  providers: [PostsService]
+})
+export class UserComponent  { 
+ name:string
+ email:string
+ address:address
+ hobbies:string[]
+ showHobbies:boolean
+ posts:Post[]
+
+  constructor(private postsService : PostsService){
+     this.name = 'John Doe'
+     this.email = 'John@gmail.com' 
+     this.address ={
+          street:'12 main st',
+          city:'Boston',
+          state:'WA'
+     }
+     this.hobbies = ['music', 'movies', 'sports'];
+     this.showHobbies=false;
+
+     this.postsService.getPosts().subscribe(posts =>{
+       this.posts=posts;
+    })
+  }
+
+  toggleHobbies(){
+    if(this.showHobbies)
+        this.showHobbies=false;
+    else
+        this.showHobbies=true;
+  }
+  addHobby(hobby:string){
+    this.hobbies.push(hobby);
+  }
+  deleteHobby(i:number){
+    this.hobbies.splice(i,1);
+  }
+}
+
+interface address{
+  street:string;
+  city:string;
+  state:string;
+}
+
+interface Post{
+  id:number
+  title:string
+  body:string
+}
